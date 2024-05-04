890a99692f5e2a86aca18646626ca08053f7f2ad
Knightsaket007 <109225097+Knightsaket007@users.noreply.github.com>
2024-05-04 11:04:07 +0530

    last commit

--- script.js
+++ script.js
@@ -44,3 +44,73 @@ function dotClick(dot) {
     curr = dotIndex;
     target.style.backgroundImage = `url('${img[curr]}')`;
 }
+
+
+
+const h1Texts = [
+    "Text 1 for h1",
+    "Text 2 for h1",
+    "Text 3 for h1",
+    
+];
+
+
+
+const strongTexts = [
+    "Kompetent. Vertrauensvoll. Wertschätzend.",
+    "Text 2 for strong",
+    "Text 3 for strong",
+];
+
+let currentVisibleIndexH1 = 0;
+let currentVisibleIndexP = 0;
+let currentVisibleIndexStrong = 0;
+
+function updateText(scrollTop) {
+    const sectionHeading = document.querySelector('.section-heading');
+    const h1Element = sectionHeading.querySelector('h1');
+    const strongElement = sectionHeading.querySelector('strong');
+
+    let visibleIndexH1 = Math.floor(scrollTop / window.innerHeight);
+    let visibleIndexStrong = Math.floor(scrollTop / window.innerHeight);
+
+    // Clamp the indices to prevent out-of-bounds access
+    visibleIndexH1 = Math.max(0, Math.min(visibleIndexH1, h1Texts.length - 1));
+    visibleIndexStrong = Math.max(0, Math.min(visibleIndexStrong, strongTexts.length - 1));
+
+    if (visibleIndexH1 !== currentVisibleIndexH1) {
+        h1Element.style.transition = 'opacity 0.6s ease-in-out';
+        h1Element.style.opacity = 0;
+
+        setTimeout(function() {
+            h1Element.textContent = h1Texts[visibleIndexH1];
+            
+            h1Element.style.opacity = 1;
+
+            currentVisibleIndexH1 = visibleIndexH1;
+        }, 600); 
+        
+    }
+
+
+    if (visibleIndexStrong !== currentVisibleIndexStrong) {
+        strongElement.style.transition = 'opacity 0.6s ease-in-out';
+        strongElement.style.opacity = 0;
+
+        setTimeout(function() {
+            strongElement.textContent = strongTexts[visibleIndexStrong];
+            
+            strongElement.style.opacity = 1;
+
+            currentVisibleIndexStrong = visibleIndexStrong;
+        }, 600); 
+    }
+}
+
+// Event listener for scroll
+window.addEventListener('scroll', function() {
+    const scrollTop = window.scrollY;
+    updateText(scrollTop);
+});
+
+updateText(window.scrollY);
